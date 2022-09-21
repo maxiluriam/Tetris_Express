22699fe71f82650f5c375f5e5c76bf2c66d89612
maxiluriam <98972327+maxiluriam@users.noreply.github.com>
2022-09-21 12:54:18 +0200

    initial push

---
+++ server.js
@@ -0,0 +1,47 @@
+// Requiring in-built https for creating
+// https server
+
+let PORT = 3002;
+
+const http = require("http");
+
+// Express for handling GET and POST request
+const express = require("express");
+
+// Requiring file system to use local files
+const fs = require("fs");
+
+// Parsing the form of body to take
+// input from forms
+const bodyParser = require("body-parser");
+
+// Configuring express to use body-parser
+// as middle-ware
+
+const app = express();
+app.use(express.json());
+app.use(express.static("express"));
+// default URL for website
+app.use("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/express/index.html"));
+  //__dirname : It will resolve to your project folder.
+});
+
+// Post request for geetting input from
+// the form
+app.post("/mssg", function (req, res) {
+  // Logging the form body
+  console.log(req.body);
+
+  // Redirecting to the root
+  res.redirect("/");
+});
+
+// Creating object of key and certificate
+// for SSL
+
+// Creating https server by passing
+// options and app object
+http.createServer(app).listen(PORT, function (req, res) {
+  console.log(`Server started at port ${PORT}`);
+});
